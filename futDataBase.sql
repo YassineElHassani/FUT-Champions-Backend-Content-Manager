4689efb6e374a0b71a5d1114f0e247f7f894e7cf
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-15 15:24:29 +0100

    creation player&position tables + foreign key

--- futDataBase.sql
+++ futDataBase.sql
@@ -16,3 +16,45 @@ CREATE TABLE nationality(
     nationalityName VARCHAR(100) NOT NULL,
     nationalityLogo VARCHAR(255) NOT NULL
 );
+
+-- Creation of the position table
+USE futDB;
+CREATE TABLE position(
+    positionID INT PRIMARY KEY AUTO_INCREMENT,
+    positionName VARCHAR(3) NOT NULL UNIQUE
+);
+
+-- Creation of the player table
+USE futDB;
+CREATE TABLE player(
+	playerID INT PRIMARY KEY AUTO_INCREMENT,
+    playerName VARCHAR(100) NOT NULL UNIQUE,
+    positionID INT NOT NULL,
+    nationalityID INT NOT NULL,
+    clubID INT NOT NULL,
+    playerPace INT NOT NULL,
+    playerShooting INT NOT NULL,
+    playerPassing INT NOT NULL,
+    playerDribbling INT NOT NULL,
+    playerDefending INT NOT NULL,
+    playerPhysical INT NOT NULL,
+    playerDiving INT NOT NULL,
+    playerHandling INT NOT NULL,
+    playerKicking INT NOT NULL,
+    playerReflexes INT NOT NULL,
+    playerSpeed INT NOT NULL,
+    playerPositioning INT NOT NULL
+);
+
+
+-- FOREIGN KEY positionID
+ALTER TABLE `player`
+ADD CONSTRAINT `fk_position` FOREIGN KEY (`positionID`) REFERENCES `position`(`positionID`);
+
+-- FOREIGN KEY nationalityID
+ALTER TABLE `player`
+ADD CONSTRAINT `fk_nationalityID` FOREIGN KEY (`nationalityID`) REFERENCES `nationality`(`nationalityID`);
+
+-- FOREIGN KEY clubID
+ALTER TABLE `player`
+ADD CONSTRAINT `fk_clubID` FOREIGN KEY (`clubID`) REFERENCES `club`(`clubID`);
