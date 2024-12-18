a029422ca3c59339588afe81f3872719b662c147
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-18 21:57:12 +0100

    editing

--- addPlayers.php
+++ addPlayers.php
@@ -109,7 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="./src/img/logo.png" type="image/x-icon"/>
-    <title>Dashboard</title>
+    <title>Add Players 👥</title>
     <link rel="stylesheet" href="./style/dashboardStyle.css">
     <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
