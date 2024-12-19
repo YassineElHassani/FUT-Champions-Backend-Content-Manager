9fe72b236b9a158d1bb32e944ee116b7857256b6
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-19 15:05:09 +0100

    Changing the the value of GK

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const standardPlayer = document.getElementById('standardPlayer');
 const gkFields = document.getElementById('gkFields');
 
 playerPosition.addEventListener('change', (e) => {
-    const isGoalkeeper = e.target.value === "1";
+    const isGoalkeeper = e.target.value === 'GK';
     standardPlayer.classList.toggle('hidden', isGoalkeeper);
     gkFields.classList.toggle('hidden', !isGoalkeeper);
 });
