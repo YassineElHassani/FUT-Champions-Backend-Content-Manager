132ca915a8f0b0719220a19b63770656f255026a
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-19 15:14:41 +0100

    Finishing the file that update all player infos to the database

---
+++ updatePlayer.php
@@ -0,0 +1,76 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "futdb";
+
+$connection = new mysqli($servername, $username, $password, $database);
+
+$errorMessage = "";
+$successMessage = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $playerID = $_POST["playerID"];
+    $playerPosition = $_POST["playerPosition"];
+    $playerName = $_POST["playerName"];
+    $playerNationality = $_POST["playerNationality"];
+    $playerClub = $_POST["playerClub"];
+    $playerPhoto = $_POST["playerPhoto"];
+    $playerRating = $_POST["playerRating"];
+
+    $playerPace = !empty($_POST["playerPace"]) ? $_POST["playerPace"] : null;
+    $playerShooting = !empty($_POST["playerShooting"]) ? $_POST["playerShooting"] : null;
+    $playerPassing = !empty($_POST["playerPassing"]) ? $_POST["playerPassing"] : null;
+    $playerDribbling = !empty($_POST["playerDribbling"]) ? $_POST["playerDribbling"] : null;
+    $playerDefending = !empty($_POST["playerDefending"]) ? $_POST["playerDefending"] : null;
+    $playerPhysical = !empty($_POST["playerPhysical"]) ? $_POST["playerPhysical"] : null;
+    $playerDiving = !empty($_POST["playerDiving"]) ? $_POST["playerDiving"] : null;
+    $playerHandling = !empty($_POST["playerHandling"]) ? $_POST["playerHandling"] : null;
+    $playerKicking = !empty($_POST["playerKicking"]) ? $_POST["playerKicking"] : null;
+    $playerReflexes = !empty($_POST["playerReflexes"]) ? $_POST["playerReflexes"] : null;
+    $playerSpeed = !empty($_POST["playerSpeed"]) ? $_POST["playerSpeed"] : null;
+    $playerPositioning = !empty($_POST["playerPositioning"]) ? $_POST["playerPositioning"] : null;
+
+    do {
+        if (empty($playerID) || empty($playerPosition) || empty($playerName) || empty($playerNationality) || empty($playerClub) || empty($playerPhoto) || empty($playerRating)) {
+            $errorMessage = "All required fields are mandatory.";
+            break;
+        }
+
+        $sql = "UPDATE player 
+                SET position = ?, 
+                    playerName = ?, 
+                    playerImage = ?, 
+                    nationalityID = ?, 
+                    clubID = ?, 
+                    playerPace = ?, 
+                    playerShooting = ?, 
+                    playerPassing = ?, 
+                    playerDribbling = ?, 
+                    playerDefending = ?, 
+                    playerPhysical = ?, 
+                    playerRating = ?, 
+                    playerDiving = ?, 
+                    playerHandling = ?, 
+                    playerKicking = ?, 
+                    playerReflexes = ?, 
+                    playerSpeed = ?, 
+                    playerPositioning = ? 
+                WHERE playerID = ?";
+        $stmt = $connection->prepare($sql);
+
+        $stmt->bind_param("sssiiiiiiiiiiiiiiii", $playerPosition, $playerName, $playerPhoto, $playerNationality, $playerClub, 
+                          $playerPace, $playerShooting, $playerPassing, $playerDribbling, $playerDefending, $playerPhysical, 
+                          $playerRating, $playerDiving, $playerHandling, $playerKicking, $playerReflexes, $playerSpeed, 
+                          $playerPositioning, $playerID);
+
+        if (!$stmt->execute()) {
+            echo "Error: " . $stmt->error;
+        } else {
+            header('Location: ./editing.php?id=' . $playerID . 'success');
+            exit;
+        }
+
+    } while (false);
+}
+?>
