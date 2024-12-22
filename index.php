1fda9cf20694618286dbd0ab40859e9c03e44190
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-22 15:57:26 +0100

    Create a static stadium

--- index.php
+++ index.php
@@ -16,11 +16,37 @@
         </nav>
     </header>
     <main class="homeContainer">
-
+        <div class="stadium">
+            <div id="firstFormation" class="flex flex-col items-center" style="margin-bottom: 20px;">
+                <div id="attackPlayer">
+                    <div class="playerSlot LF"></div>
+                    <div class="playerSlot ST"></div>
+                    <div class="playerSlot RF"></div>
+                </div>
+                <div id="midPlayer">
+                    <div class="playerSlot LCM"></div>
+                    <div class="playerSlot CAM"></div>
+                    <div class="playerSlot RCM"></div>
+                </div>
+                <div id="defencePlayer">
+                    <div style="display: flex; justify-content: space-around; width: 100%;">
+                        <div class="playerSlot LB"></div>
+                        <div class="playerSlot LCB"></div>
+                    </div>
+                    <div style="display: flex; justify-content: space-around; width: 100%;">
+                        <div class="playerSlot RCB"></div>
+                        <div class="playerSlot RB"></div>
+                    </div>
+                </div>
+                <div id="gkPlayer">
+                    <div class="playerSlot GK"></div>
+                </div>
+            </div>
+        </div>
     </main>
-    <footer>
-
+    <footer class="text-center py-4 bg-gray-900 text-gray-500 h-[200px]">
+        <p class="pt-[80px]">© 2024 FUT Champions Squad Builder. All Rights Reserved.</p>
     </footer>
-    <script src="./script/script.js"></script>
+    <script src=""></script>
 </body>
 </html>
